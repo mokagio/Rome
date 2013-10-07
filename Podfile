1ebd53edefaf5debe9e14316a4c0226e849c7b6c
Gio <giovanni.lodi42@gmail.com>
2013-10-07 23:34:39 +0100

    Pods, xctool, and Specs setup

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, "7.0"
+
+target "Rome" do
+
+end
+
+target "RomeTests", :exclusive => true do
+	pod 'Kiwi'
+end
+
---
+++ RomeTests/RMRouterSpec.m
@@ -0,0 +1,19 @@
+//
+//  RMRouterSpec.m
+//  Rome
+//
+//  Created by Gio on 07/10/2013.
+//  Copyright (c) 2013 mokagio. All rights reserved.
+//
+
+#import <Kiwi.h>
+
+SPEC_BEGIN(MRRouterSpec)
+
+describe(@"MRRouter", ^{
+    it(@"has as spec that's here only to see if the tests run", ^{
+        [[@YES should] beTrue];
+    });
+});
+
+SPEC_END
