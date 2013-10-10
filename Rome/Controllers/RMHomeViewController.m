face362e3ff9cc6d4f3e0b5634dc6af0e917b2b6
Gio <giovanni.lodi42@gmail.com>
2013-10-10 08:53:29 +0100

    Scheduled local notification that we'll use to test the routing

--- Rome/Controllers/RMHomeViewController.m
+++ Rome/Controllers/RMHomeViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "RMHomeViewController.h"
+#import <UIAlertView+Blocks.h>
 
 @interface RMHomeViewController ()
 - (IBAction)redButtonTouched:(id)sender;
 - (IBAction)blueButtonTouched:(id)sender;
+- (void)scheduleLocalNotificationWithColor:(NSString *)color;
 @end
 
 @implementation RMHomeViewController
@@ -22,9 +24,39 @@
 }
 
 - (IBAction)redButtonTouched:(id)sender {
+    [UIAlertView showWithTitle:@"YOU CHOSE RED"
+                       message:@"now close the app and in about 15 seconds you'll receive a notification"
+             cancelButtonTitle:@"OK"
+             otherButtonTitles:@[]
+                      tapBlock:^(UIAlertView *alertView, NSInteger buttonIndex) {
+                          [self scheduleLocalNotificationWithColor:@"red"];
+                      }];
 }
 
 - (IBAction)blueButtonTouched:(id)sender {
+    [UIAlertView showWithTitle:@"YOU CHOSE BLUE"
+                       message:@"now close the app and in about 15 seconds you'll receive a notification"
+             cancelButtonTitle:@"OK"
+             otherButtonTitles:@[]
+                      tapBlock:^(UIAlertView *alertView, NSInteger buttonIndex) {
+                          [self scheduleLocalNotificationWithColor:@"blue"];
+                      }];
+}
+
+- (void)scheduleLocalNotificationWithColor:(NSString *)color
+{
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    if (notification == nil) {
+        // notify the user!
+        return;
+    } else {
+        notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:15];
+        notification.timeZone = [NSTimeZone defaultTimeZone];
+        notification.alertBody = @"Time to see where the pill brought you";
+        notification.applicationIconBadgeNumber = 1;
+        notification.userInfo = @{@"path":color};
+        [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+    }
 }
 
 @end
